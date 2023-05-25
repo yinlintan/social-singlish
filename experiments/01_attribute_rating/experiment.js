2b30ee952b3eb3478b79352a9fb928b5f486c97d
tylanin <62355072+tylanin@users.noreply.github.com>
2023-05-24 22:57:29 -0700

    pause collection

--- experiments/01_attribute_rating/experiment.js
+++ experiments/01_attribute_rating/experiment.js
@@ -16,6 +16,13 @@ const preload_trial = {
     audio: preload_array
 };
 
+const stopCollection = {
+    type: jsPsychHtmlKeyboardResponse,
+    stimulus: '<p>This study is currently not accepting new participants. Please check back again in the future.</p>',
+    choices: "NO_KEYS",
+};
+timeline.push(stopCollection);
+
 const irb = {
     type: jsPsychHtmlButtonResponse,
     stimulus: `
